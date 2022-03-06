5f3137ed1c6445efa8306bcf26eb8d8598d751fe
hoonjoo94 <hoonjoo94@naver.com>
2022-03-07 04:44:40 +0900

    🎉 Gatsby Blog Init

---
+++ src/components/Footer.tsx
@@ -0,0 +1,89 @@
+import { Link } from 'gatsby';
+import { setLightness } from 'polished';
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+import { colors } from '../styles/colors';
+import { outer, inner } from '../styles/shared';
+import config from '../website-config';
+
+export const Footer: React.FC = () => (
+  <footer css={[outer, SiteFooter]}>
+    <div css={[inner, SiteFooterContent]}>
+      <section className="copyright">
+        <Link to="/">{config.title}</Link> &copy; {new Date().getFullYear()}{' '}
+        {config.footer && (
+          <Link to="/">
+            | {config.title} {config.footer}
+          </Link>
+        )}
+      </section>
+      <SiteFooterNav>
+        <Link to="/">Latest Posts</Link>
+        {config.github && (
+          <a href={config.github} target="_blank" rel="noopener noreferrer">
+            Github
+          </a>
+        )}
+      </SiteFooterNav>
+    </div>
+  </footer>
+);
+
+const SiteFooter = css`
+  position: relative;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  color: #fff;
+  background: ${setLightness('0.0015', colors.darkgrey)};
+`;
+
+const SiteFooterContent = css`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  color: rgba(255, 255, 255, 0.7);
+  font-size: 1.5rem;
+  a {
+    color: rgba(255, 255, 255, 0.7);
+  }
+  a:hover {
+    color: rgba(255, 255, 255, 1);
+    text-decoration: none;
+  }
+  @media (max-width: 650px) {
+    flex-direction: column;
+  }
+`;
+
+const SiteFooterNav = styled.nav`
+  display: flex;
+
+  a {
+    position: relative;
+    margin-left: 20px;
+  }
+
+  a:before {
+    content: '';
+    position: absolute;
+    top: 11px;
+    left: -11px;
+    display: block;
+    width: 2px;
+    height: 2px;
+    background: #fff;
+    border-radius: 100%;
+  }
+
+  a:first-of-type:before {
+    display: none;
+  }
+  @media (max-width: 650px) {
+    a:first-of-type {
+      margin-left: 0;
+    }
+  }
+`;
