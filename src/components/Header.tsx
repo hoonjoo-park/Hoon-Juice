7fbf10acf1f6793242d5e334869a7aa35c7c8ae5
hoonjoo94 <hoonjoo94@naver.com>
2023-01-26 02:18:34 +0900

    Pretendard 폰트 적용 -> 헤더 마크업

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header
+      className={
+        'h-16 flex items-center justify-center border-b border-gray-600 '
+      }
+    >
+      <nav className={'max-w-6xl w-11/12 flex items-center justify-between'}>
+        <h2 className={'font-bold text-xl'}>훈주스 블로그</h2>
+        <div>
+          <ul className={'flex items-center'}>
+            <li className={'font-semibold text-base'}>
+              <Link
+                className={'px-4 hover:text-blue-500 transition'}
+                href={'/posts/web'}
+              >
+                웹 개발
+              </Link>
+            </li>
+            <li className={'font-semibold text-base'}>
+              <Link
+                className={'px-4 hover:text-blue-500 transition'}
+                href={'/posts/app'}
+              >
+                앱 개발
+              </Link>
+            </li>
+            <li className={'font-semibold text-base'}>
+              <Link
+                className={'px-4 hover:text-blue-500 transition'}
+                href={'/about'}
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import Header from '@/components/Header'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
