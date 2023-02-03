737a96642deb4cd26d6bca0e6e2d46476b54d8e8
hoonjoo94 <hoonjoo94@naver.com>
2023-02-04 02:56:34 +0900

    이미지 최적화 개선

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
 const withOptimizedImages = require('next-optimized-images')
 
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  ...nextConfig,
-  ...withOptimizedImages({ handleImages: ['jpeg', 'png', 'svg'] }),
-}
+module.exports = nextConfig
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      images: {
+        handleImages: ['jpeg', 'png', 'svg'],
+      },
+    },
+  ],
+])
+
+module.exports = withOptimizedImages({})
