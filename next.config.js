e740ef5bfcf2091780cfe79e1b231dfefad7dfce
hoonjoo-park <hoonjoo94@naver.com>
2025-04-13 00:21:29 +0900

    next.config 및 package.json 최적화

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
-const withOptimizedImages = require('next-optimized-images')
-
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 }
 
 module.exports = nextConfig
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      images: {
-        handleImages: ['jpeg', 'png', 'svg', 'webp'],
-      },
-    },
-  ],
-])
-
-module.exports = withOptimizedImages({})
